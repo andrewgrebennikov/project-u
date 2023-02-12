f02512e18f672dcf90fcc509f75615a231bfb695
Andrey Grebennikov <lostplanet08@mail.ru>
2023-02-12 23:06:21 +0400

    1. Настроил роуты

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,8 @@
-import { Suspense } from 'react';
 import classNames from 'classnames';
-import { Link, Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useTheme } from 'app/providers/ThemeProvider';
-import { MainPage } from 'pages/MainPage';
-import { AboutPage } from 'pages/AboutPage';
 import 'app/styles/index.scss';
+import { AppRouter } from 'app/router';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -16,12 +14,7 @@ const App = () => {
         <Link to="/">Главная</Link>
         <Link to="/about">О нас</Link>
       </nav>
-      <Suspense fallback={<div>Загрузка...</div>}>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
