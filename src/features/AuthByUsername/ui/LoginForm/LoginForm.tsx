c9eef2e5210b7fe1b45a436f656bdf3d2ed8a948
Andrey Grebennikov <lostplanet08@mail.ru>
2023-04-06 11:50:52 +0400

    1. Добавил форму авторизации

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { Input } from 'shared/ui/Input/Input';
 import { Button } from 'shared/ui/Button/Button';
 import styles from './LoginForm.module.scss';
 
 export const LoginForm = () => {
   const [name, setName] = useState('');
+  const { t } = useTranslation();
 
   const handleNameChange = (value: string) => {
     setName(value);
@@ -13,19 +15,19 @@ export const LoginForm = () => {
   return (
     <form className={styles.form}>
       <fieldset className={styles.fieldset}>
-        <legend className={styles.legend}>Форма авторизации</legend>
+        <legend className={styles.legend}>{t('Форма авторизации')}</legend>
         <Input
           className={styles.input}
           type="text"
-          label="Введите имя"
+          label={t('Имя')}
           value={name}
           onChange={handleNameChange}
           autoFocus
         />
-        <Input className={styles.input} type="text" label="Введите пароль" />
+        <Input className={styles.input} type="text" label={t('Пароль')} />
       </fieldset>
       <div className={styles.actions}>
-        <Button variant="outlined">Отправить</Button>
+        <Button variant="outlined">{t('Отправить')}</Button>
       </div>
     </form>
   );
