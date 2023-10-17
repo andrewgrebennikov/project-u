14a31096363aa8c3d58dbe7e9b95be7f861b4a0d
Andrey Grebennikov <lostplanet08@mail.ru>
2023-10-17 13:29:27 +0400

    1. Добавил страницу профиля

--- src/pages/ProfilePage/ui/ProfilePage.stories.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import ProfilePage from './ProfilePage';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator';
 
 const meta: Meta<typeof ProfilePage> = {
   component: ProfilePage,
@@ -13,10 +14,10 @@ type Story = StoryObj<typeof ProfilePage>;
 
 export const Light: Story = {
   args: {},
-  decorators: [ThemeDecorator(Theme.LIGHT)],
+  decorators: [ThemeDecorator(Theme.LIGHT), StoreDecorator({})],
 };
 
 export const Dark: Story = {
   args: {},
-  decorators: [ThemeDecorator(Theme.DARK)],
+  decorators: [ThemeDecorator(Theme.DARK), StoreDecorator({})],
 };
