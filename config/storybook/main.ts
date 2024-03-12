cc9d70e21f8b003182969afe759432c0ad701f37
Andrey Grebennikov <lostplanet08@mail.ru>
2024-03-13 00:37:27 +0400

    1. Обновил storybook

--- config/storybook/main.ts
+++ config/storybook/main.ts
@@ -3,14 +3,26 @@ import type { StorybookConfig } from '@storybook/react-webpack5';
 const config: StorybookConfig = {
   stories: ['../../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
+    '@storybook/addon-webpack5-compiler-swc',
+    '@storybook/addon-onboarding',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
     '@storybook/addon-interactions',
     '@storybook/addon-themes',
   ],
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
+  },
+  swc: {
+    jsc: {
+      transform: {
+        react: {
+          runtime: 'automatic',
+        },
+      },
+    },
   },
   staticDirs: ['../../public'],
   docs: {
